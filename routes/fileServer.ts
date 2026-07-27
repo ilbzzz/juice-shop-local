cbb92a90a6edabc6face1eb6423d15cc2cc7e613
CodeMender Agent <codemender-agent@google.com>
2026-07-27 21:48:50 +0000

    fix(security): resolve Path Traversal in /app/juice-shop-local/routes/fileServer.ts

--- routes/fileServer.ts
+++ routes/fileServer.ts
@@ -24,9 +24,8 @@ export function servePublicFiles () {
   }
 
   function verify (file: string, res: Response, next: NextFunction) {
+    file = security.cutOffPoisonNullByte(file)
     if (file && (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
-      file = security.cutOffPoisonNullByte(file)
-
       challengeUtils.solveIf(challenges.directoryListingChallenge, () => { return file.toLowerCase() === 'acquisitions.md' })
       verifySuccessfulPoisonNullByteExploit(file)
 
